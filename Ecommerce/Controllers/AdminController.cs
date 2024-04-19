ad3cb910858c5e0229d4b24fe845575c59e99051
tanvirhossainapon <tanvirhossainapon@gmail.com>
2024-04-19 23:54:08 +0600

    admin log and category feature in working

---
+++ Ecommerce/Controllers/AdminController.cs
@@ -0,0 +1,149 @@
+﻿using Ecommerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+
+namespace Ecommerce.Controllers
+{
+    public class AdminController : Controller
+    {
+        dbmarketingEntities db = new dbmarketingEntities();
+        // GET: Admin
+        [HttpGet]
+        public ActionResult login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult login(tbl_admin avm )
+        {
+            tbl_admin ad = db.tbl_admin.Where(x => x.ad_username == avm.ad_username && x.ad_password == avm.ad_password).SingleOrDefault();
+            if(ad!=null)
+            {
+                Session["ad_id"] = ad.ad_id.ToString();
+                return RedirectToAction("Create");
+            }
+            else
+            {
+                ViewBag.error = "Invalid username or password";
+
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            if (Session["ad_id"]==null)
+            {
+                return RedirectToAction("login");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(tbl_category cvm,HttpPostedFileBase imgfile)
+        {
+            string path = uploadingimgfile(imgfile);
+
+           if(path.Equals("-1"))
+            {
+
+                ViewBag.error = "Image could not be uploaded";
+            }
+
+            else
+            {
+                tbl_category cat = new tbl_category();
+                cat.cat_name = cvm.cat_name;
+                cat.cat_image = path;
+                cat.cat_status = 1;
+                cat.cat_fk_ad = Convert.ToInt32(Session["ad_id"].ToString());
+                db.tbl_category.Add(cat);
+                db.SaveChanges();
+
+                return RedirectToAction("Create");
+
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult ViewCategory()
+        {
+            return View();
+        }
+
+
+
+
+
+
+
+
+        //image uploader code
+        public string uploadingimgfile(HttpPostedFileBase file)
+        {
+            Random r = new Random();
+            string path = "-1";
+            int random = r.Next();
+            if(file != null && file.ContentLength>0)
+            {
+
+                string extension = Path.GetExtension(file.FileName);
+
+                if (extension.ToLower().Equals(".jpg") || extension.ToLower().Equals(".jpeg") || extension.ToLower().Equals(".png"))
+                {
+
+                    try
+                    {
+
+                        path = Path.Combine(Server.MapPath("~/Content/upload"), random + Path.GetFileName(file.FileName));
+                        file.SaveAs(path);
+
+                        path = "~/Content/upload" + random + Path.GetFileName(file.FileName);
+
+                    }
+
+                    catch (Exception ex)
+                    {
+
+                        path = "-1";
+
+                    }
+
+                }
+
+                else
+                {
+                    Response.Write("<script> alert('Only jpg, jpeg or png formats are acceptable')</script>");
+                }
+
+            }
+
+            else
+            { 
+
+                Response.Write("<script>alert('Please select a file')</script>");
+                path = "-1";
+
+            }
+
+
+
+
+            return path;
+
+
+
+
+        }
+
+    }
+}
